6f2c5e29674faac8180a2fdc8c53e333e13fcbfb
Sabra <52722031+sabrasisler@users.noreply.github.com>
2022-02-07 10:07:15 -0500

    added get instance for tuit controller to get an instance in the server.ts file

--- controllers/TuitController.ts
+++ controllers/TuitController.ts
@@ -4,34 +4,36 @@ import TuitControllerI from "../interfaces/TuitController";
 import Tuit from "../models/Tuit";
 
 export default class TuitController implements TuitControllerI {
-    app: Express;
-    tuitDao: TuitDao;
-    constructor(app: Express, tuitDao: TuitDao) {
-        this.app = app;
-        this.tuitDao = tuitDao;
-        this.app.get('/tuits', this.findAllTuits);
-        this.app.get('/tuits/:tid', this.findTuitById);
-        this.app.get('/users/:uid/tuits', this.findTuitsByUser);
-        this.app.post('/tuits', this.createTuit);
-        this.app.delete('/tuits/:tid', this.deleteTuit);
-        this.app.put('/tuit/:tid', this.updateTuit);
+    private static tuitDao: TuitDao = TuitDao.getInstance();
+    private static tuitController: TuitController | null = null;
+    public static getInstance = (app: Express): TuitController => {
+        if (TuitController.tuitController === null) {
+            TuitController.tuitController = new TuitController();
+            app.get('/tuits', TuitController.tuitController.findAllTuits);
+            app.get('/tuits/:tid', TuitController.tuitController.findTuitById);
+            app.get('/users/:uid/tuits', TuitController.tuitController.findTuitsByUser);
+            app.post('/tuits', TuitController.tuitController.createTuit);
+            app.delete('/tuits/:tid', TuitController.tuitController.deleteTuit);
+            app.put('/tuit/:tid', TuitController.tuitController.updateTuit);
+        }
+        return TuitController.tuitController;
     }
     findAllTuits = (req: Request, res: Response) =>
-        this.tuitDao.findAllTuits()
+        TuitController.tuitDao.findAllTuits()
             .then(tuits => res.json(tuits));
     findTuitById = (req: Request, res: Response) =>
-        this.tuitDao.findTuitById(req.params.tid)
+        TuitController.tuitDao.findTuitById(req.params.tid)
             .then(tuit => res.json(tuit));
     findTuitsByUser = (req: Request, res: Response) =>
-        this.tuitDao.findTuitsByUser(req.params.uid)
+        TuitController.tuitDao.findTuitsByUser(req.params.uid)
             .then((tuits: Tuit[]) => res.json(tuits));
     createTuit = (req: Request, res: Response) =>
-        this.tuitDao.createTuit(req.body)
+        TuitController.tuitDao.createTuit(req.body)
             .then(tuit => res.json(tuit));
     deleteTuit = (req: Request, res: Response) =>
-        this.tuitDao.deleteTuit(req.params.tid)
+        TuitController.tuitDao.deleteTuit(req.params.tid)
             .then(status => res.json(status));
     updateTuit = (req: Request, res: Response) =>
-        this.tuitDao.updateTuit(req.params.tid, req.body)
+        TuitController.tuitDao.updateTuit(req.params.tid, req.body)
             .then(status => res.json(status));
 }
--- server.ts
+++ server.ts
@@ -1,11 +1,12 @@
 import express, {Request, Response} from 'express';
 import UserController from "./controllers/UserController";
+import TuitController from './controllers/TuitController';
 import mongoose from "mongoose";
 
-
-mongoose.connect('mongodb://localhost:27017/cs1234');
+mongoose.connect("mongodb+srv://ssisler:securepassword@cluster0.q2crm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
 const app = express();
 app.use(express.json());
 const userController = UserController.getInstance(app);
+const tuitController = TuitController.getInstance(app);
 const PORT = 4000;
 app.listen(process.env.PORT || PORT);
