e46d6f2157e50a13d8638ba010be484e5a386995
Sabra <52722031+sabrasisler@users.noreply.github.com>
2022-02-06 21:46:42 -0500

    added controllers, data models, and first attempt at DAO

---
+++ daos/TuitDao.ts
@@ -0,0 +1,40 @@
+import Tuit from "../models/Tuit";
+import TuitModel from "../mongoose/TuitModel";
+import TuitSchema from "../mongoose/TuitSchema";
+import TuitDaoI from "../interfaces/TuitDao";
+
+export default class TuitDao implements TuitDaoI {
+
+    private static tuitDao: TuitDao | null = null;
+    public static getInstance = (): TuitDao => {
+        if (TuitDao.tuitDao === null) {
+            TuitDao.tuitDao = new TuitDao();
+        }
+        return TuitDao.tuitDao;
+    }
+
+    private constructor() { }
+
+    async findAllTuits(): Promise<Tuit[]> {
+        return await TuitModel.find();
+    }
+    async findTuitsByUser(uid: string): Promise<Tuit[]> {
+        return await TuitModel.find({postedBy: uid});
+    }
+    async findTuitById(tid: string): Promise<Tuit> {
+        return await TuitModel.findById(tid));
+    }
+    async createTuit(tuit: Tuit): Promise<Tuit> {
+        return await TuitModel.create(tuit);
+    }
+
+    async updateTuit(tid: string, tuit: Tuit): Promise<any> {
+        return await TuitModel.updateOne({ _id: tid }, { $set: tuit });
+    }
+
+    async deleteTuit(tid: string): Promise<any> {
+        return await TuitModel.deleteOne({ _id: tid });
+    }
+
+}
+
--- server.ts
+++ server.ts
@@ -1,11 +1,11 @@
 import express, {Request, Response} from 'express';
+import UserController from "./controllers/UserController";
+import mongoose from "mongoose";
+
+
+mongoose.connect('mongodb://localhost:27017/cs1234');
 const app = express();
-
-app.get('/hello', (req: Request, res: Response) =>
-    res.send('Hello World!'));
-
-app.get('/add/:a/:b', (req: Request, res: Response) =>
-    res.send(req.params.a + req.params.b));
-
+app.use(express.json());
+const userController = UserController.getInstance(app);
 const PORT = 4000;
 app.listen(process.env.PORT || PORT);
