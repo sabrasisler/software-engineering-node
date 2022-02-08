dfa8de4946bdfe84f4d4b636a721341782c8c6f3
Sabra <52722031+sabrasisler@users.noreply.github.com>
2022-02-08 15:25:16 -0500

    correct tuit model

--- models/Tuit.ts
+++ models/Tuit.ts
@@ -4,5 +4,5 @@ import User from "./User";
 export default class Tuit {
    private tuit: string = '';
    private postedOn: Date = new Date();
-   private postedBy: Types.ObjectId | null = null;
+   private postedBy: User | null = null;
 }
