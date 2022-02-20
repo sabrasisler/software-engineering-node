84d9d35d69ef9cc606131fcfc389c67a2b0b5557
Sabra <52722031+sabrasisler@users.noreply.github.com>
2022-02-20 18:56:48 -0500

    setup users, likes, and tuits from the starter code

--- server.ts
+++ server.ts
@@ -3,7 +3,7 @@ import UserController from "./controllers/UserController";
 import TuitController from './controllers/TuitController';
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://ssisler:securepassword@cluster0.uwi8v.mongodb.net/Tuiter?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://sabrasisler:securepassword@tuitera2.nghpg.mongodb.net/TuiterA2?retryWrites=true&w=majority");
 const app = express();
 app.use(express.json());
 const userController = UserController.getInstance(app);
