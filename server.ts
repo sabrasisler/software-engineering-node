a4eaf7e14090d5f154890ff3942b3ba1f2ddc52b
Sabra <52722031+sabrasisler@users.noreply.github.com>
2022-02-21 10:47:59 -0500

    added likecontroller to server.ts

--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
 import express, {Request, Response} from 'express';
 import UserController from "./controllers/UserController";
 import TuitController from './controllers/TuitController';
+import LikeController from "./controllers/LikeController";
 import mongoose from "mongoose";
 
 mongoose.connect("mongodb+srv://sabrasisler:securepassword@tuitera2.nghpg.mongodb.net/TuiterA2?retryWrites=true&w=majority");
@@ -8,5 +9,6 @@ const app = express();
 app.use(express.json());
 const userController = UserController.getInstance(app);
 const tuitController = TuitController.getInstance(app);
+const likeController = LikeController.getInstance(app);
 const PORT = 4000;
 app.listen(process.env.PORT || PORT);
