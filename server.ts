b22955ce5a20c37960f7de486cd93f87e579f3b8
Sabra <52722031+sabrasisler@users.noreply.github.com>
2022-03-25 16:39:27 -0400

    changed connection string

--- server.ts
+++ server.ts
@@ -23,15 +23,15 @@ const cors = require("cors");
 const session = require("express-session");
 
 // build the connection string
-// const PROTOCOL = "mongodb+srv";
-// const DB_USERNAME = process.env.DB_USERNAME;
-// const DB_PASSWORD = process.env.DB_PASSWORD;
-// const HOST = "cluster0.m8jeh.mongodb.net";
-// const DB_NAME = "myFirstDatabase";
-// const DB_QUERY = "retryWrites=true&w=majority";
-//const connectionString = `${PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${HOST}/${DB_NAME}?${DB_QUERY}`;// connect to the database
-//mongoose.connect(connectionString);
-mongoose.connect("mongodb+srv://sabrasisler:securepassword@tuitera2.nghpg.mongodb.net/TuiterA2?retryWrites=true&w=majority");
+const PROTOCOL = "mongodb+srv";
+const DB_USERNAME = "sabrasisler";
+const DB_PASSWORD = "securepassword";
+const HOST = "cluster0.m8jeh.mongodb.net";
+const DB_NAME = "tuitera2";
+const DB_QUERY = "retryWrites=true&w=majority";
+const connectionString = `${PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${HOST}/${DB_NAME}?${DB_QUERY}`;// connect to the database
+mongoose.connect(connectionString);
+// mongoose.connect("mongodb+srv://sabrasisler:securepassword@tuitera2.nghpg.mongodb.net/TuiterA2?retryWrites=true&w=majority");
 
 
 
