05ea21c381b0666c79ff68b74eebf54264f7d7ff
Sabra <52722031+sabrasisler@users.noreply.github.com>
2022-03-27 15:25:10 -0400

    added jsdoc for dislike controller interface

--- interfaces/DislikeControllerI.ts
+++ interfaces/DislikeControllerI.ts
@@ -1,3 +1,7 @@
+/**
+ * @file define the Controller interface for RESTful Web service API for dislikes resource
+ */
+
 import {Request, Response} from "express";
 
 export default interface DislikeControllerI {
