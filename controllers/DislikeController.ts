ec08b1f66cdcc4fac0b0b8586a04ad161030c279
Sabra <52722031+sabrasisler@users.noreply.github.com>
2022-03-27 15:24:00 -0400

    added documentation to dislikes controller

--- controllers/DislikeController.ts
+++ controllers/DislikeController.ts
@@ -1,5 +1,5 @@
 /**
- * @file Controller RESTful Web service API for likes resource
+ * @file Controller RESTful Web service API for dislikes resource
  */
  import {Express, Request, Response} from "express";
  import DislikeDao from "../daos/DislikeDao";
@@ -10,17 +10,17 @@
   * @class DislikeController Implements RESTful Web service API for dislikes resource.
   * Defines the following HTTP endpoints:
   * <ul>
-  *     <li>GET /api/users/:uid/likes to retrieve all the tuits liked by a user
+  *     <li>GET /api/users/:uid/dislikes to retrieve all the tuits disliked by a user
   *     </li>
-  *     <li>GET /api/tuits/:tid/likes to retrieve all users that liked a tuit
+  *     <li>GET /api/tuits/:tid/dislikes to retrieve all users that disliked a tuit
   *     </li>
-  *     <li>POST /api/users/:uid/likes/:tid to record that a user likes a tuit
+  *     <li>POST /api/users/:uid/dislikes/:tid to record that a user dislikes a tuit
   *     </li>
-  *     <li>DELETE /api/users/:uid/unlikes/:tid to record that a user
-  *     no londer likes a tuit</li>
+  *     <li>DELETE /api/users/:uid/undislikes/:tid to record that a user
+  *     no longer dislikes a tuit</li>
   * </ul>
-  * @property {LikeDao} likeDao Singleton DAO implementing likes CRUD operations
-  * @property {LikeController} LikeController Singleton controller implementing
+  * @property {DislikeDao} dislikeDao Singleton DAO implementing dislikes CRUD operations
+  * @property {DislikeController} DislikeController Singleton controller implementing
   * RESTful Web service API
   */
  export default class DislikeController implements DislikeControllerI {
@@ -31,7 +31,7 @@
       * Creates singleton controller instance
       * @param {Express} app Express instance to declare the RESTful Web service
       * API
-      * @return TuitController
+      * @return DislikeController
       */
      public static getInstance = (app: Express): DislikeController  => {
          if(DislikeController.dislikeController === null) {
@@ -46,9 +46,9 @@
      private constructor() {}
  
      /**
-      * Retrieves all users that liked a tuit from the database
+      * Retrieves all users that disliked a tuit from the database
       * @param {Request} req Represents request from client, including the path
-      * parameter tid representing the liked tuit
+      * parameter tid representing the disliked tuit
       * @param {Response} res Represents response to client, including the
       * body formatted as JSON arrays containing the user objects
       */
@@ -57,11 +57,11 @@
              .then(dislikes => res.json(dislikes));
  
      /**
-      * Retrieves all tuits liked by a user from the database
+      * Retrieves all tuits disliked by a user from the database
       * @param {Request} req Represents request from client, including the path
-      * parameter uid representing the user liked the tuits
+      * parameter uid representing the user disliked the tuits
       * @param {Response} res Represents response to client, including the
-      * body formatted as JSON arrays containing the tuit objects that were liked
+      * body formatted as JSON arrays containing the tuit objects that were disliked
       */
      findAllTuitsDislikedByUser = (req: Request, res: Response) => {
          const uid = req.params.uid;
@@ -81,8 +81,8 @@
      
      /**
       * @param {Request} req Represents request from client, including the
-      * path parameters uid and tid representing the user that is liking the tuit
-      * and the tuit being liked
+      * path parameters uid and tid representing the user that is disliking the tuit
+      * and the tuit being disliked
       * @param {Response} res Represents response to client, including the
       * body formatted as JSON containing the new likes that was inserted in the
       * database
